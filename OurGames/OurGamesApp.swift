069d0a94b2df088eddd3c750b22233e2ab7bd35e
Luthfi Alfarisi <luthfi.alfarisi@dansmultipro.com>
2021-07-10 16:55:23 +0700

    Initial Commit

---
+++ OurGames/OurGamesApp.swift
@@ -0,0 +1,17 @@
+//
+//  OurGamesApp.swift
+//  OurGames
+//
+//  Created by Dans Multipro on 10/07/21.
+//
+
+import SwiftUI
+
+@main
+struct OurGamesApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
